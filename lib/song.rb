4937b79735198a489cd83d8e53bac7d9390e0cca
Jamal Gelin <gelin.jamal@gmail.com>
2020-10-08 01:42:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,28 +8,6 @@ class Song
     @genre = genre
   end
 
-  def name=(name)
-    @name = name
-  end
 
-  def name
-    @name
-  end
-
-  def artist=(artist)
-    @artist = artist
-  end
-
-  def artist
-    @artist
-  end
-
-  def genre=(genre)
-    @genre
-  end
-
-  def genre
-    @genre
-  end
 
 end
