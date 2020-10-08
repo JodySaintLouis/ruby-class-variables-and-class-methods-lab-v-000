81c41f4681cd4ef9341d5fe188c920799be49dab
Jamal Gelin <gelin.jamal@gmail.com>
2020-10-08 01:54:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,6 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@count = 0
-  @@artists =
-  @@genres =
-
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
@@ -13,7 +9,7 @@ class Song
   end
 
   def count
-    
+    Sonfg.count
   end
 
 
